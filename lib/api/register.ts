4ba6ee76958329794659d2baf0a9dc7120754691
Tom Cunningham <tcunningham12@qub.ac.uk>
2023-11-19 17:26:47 +0000

    Add login lambda, add register lambda, connect to database

--- lib/api/register.ts
+++ lib/api/register.ts
@@ -3,6 +3,17 @@ import { LambdaResponseType } from '../types/response-type'
 import { createConnection, createPool } from 'mysql2';
 import { databaseName as database } from '../config/constants';
 
+/**
+ * 
+ * @param event { 
+ *   "username": "",
+ *   "password": "",
+ *   "email": "",
+ *   "firstName": "",
+ *   "lastName": ""
+ * }
+ * @returns 
+ */
 export const handler = async (event: any): Promise<LambdaResponseType> => {
 
   const requestBody = JSON.parse(event.body)
@@ -28,9 +39,26 @@ export const handler = async (event: any): Promise<LambdaResponseType> => {
     database
   }
 
+  const username = requestBody.username;
+  const firstName = requestBody.firstName;
+  const lastName = requestBody.lastName;
+  const email = requestBody.email;
+  const userPassword = requestBody.password;
+
+  if (!username || !firstName || !lastName || !email || !userPassword) {
+    return {
+        statusCode: 400,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({error: 'Missing data'}),
+      }
+  }
+
   const conn = createPool(dbConfig).promise();
 
-  const query = `SELECT * FROM Users WHERE Username = "${requestBody.username}" AND Password = "${requestBody.password}"`;
+  const query = `INSERT INTO \`Users\` ( \`Username\`, \`FirstName\`, \`LastName\`, \`Email\`, \`RegDate\`, \`Password\` ) VALUES 
+  ('${username}', '${firstName}', '${lastName}', '${email}', CURDATE(), '${userPassword}'); `
 
   try {
     const connection = await conn.getConnection();
@@ -48,7 +76,7 @@ export const handler = async (event: any): Promise<LambdaResponseType> => {
     }
   } catch (error) {
     return {
-      statusCode: 200,
+      statusCode: 500,
       headers: {
         'Content-Type': 'application/json'
       },
--- lib/team11-backend-stack.ts
+++ lib/team11-backend-stack.ts
@@ -7,6 +7,7 @@ import {
   Duration,
   Stack,
   StackProps,
+  aws_lambda_nodejs,
 } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 
@@ -14,39 +15,13 @@ export class Team11BackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    // Iam Roles
-
+    
     // Virtual Private Cloud
 
     const vpc = aws_ec2.Vpc.fromLookup(this, 'vpc', {
       isDefault:true
     })
 
-    //  ------ Lambda Functions -------
-
-    // Health lambda
-
-    const healthLambda = new aws_lambda.Function(this, 'backend-health', {
-      runtime: aws_lambda.Runtime.NODEJS_18_X,
-      handler: 'health.handler',
-      code: aws_lambda.Code.fromAsset('lib/api'),
-    })
-
-    const healthLambdaIntegration = new aws_apigateway.LambdaIntegration(
-      healthLambda
-    )
-
-    // Login Lambda
-
-    const loginLambda = new aws_lambda.Function(this, 'backend-login', {
-      runtime: aws_lambda.Runtime.NODEJS_18_X,
-      handler: 'login.handler',
-      code: aws_lambda.Code.fromAsset('lib/api'),
-    })
-
-    const loginLambdaIntegration = new aws_apigateway.LambdaIntegration(
-      loginLambda
-    )
 
     // Secret Value
 
@@ -99,7 +74,64 @@ export class Team11BackendStack extends Stack {
           subnetType: aws_ec2.SubnetType.PUBLIC,
         },
       }
-    )
+    );
+
+    // Get Secret
+
+    const secret = aws_secretsmanager.Secret.fromSecretAttributes(this, "ImportedSecret", {
+      secretCompleteArn:
+        "arn:aws:secretsmanager:eu-west-1:394261647652:secret:databasesecret6A44CD8F-Wk9XSvKVBbLc-cjE3XH"
+      // If the secret is encrypted using a KMS-hosted CMK, either import or reference that key:
+      // encryptionKey: ...
+    });
+
+    
+
+    //  ------ Lambda Functions -------
+
+    const healthLambda = new aws_lambda_nodejs.NodejsFunction(this, 'backend-health', {
+      runtime: aws_lambda.Runtime.NODEJS_18_X,
+      entry: 'lib/api/health.ts',
+      handler: 'handler',
+    });
+
+    const healthLambdaIntegration = new aws_apigateway.LambdaIntegration(
+      healthLambda
+    );
+
+    // Login Lambda
+
+    const loginLambda = new aws_lambda_nodejs.NodejsFunction(this, 'backend-login', {
+      runtime: aws_lambda.Runtime.NODEJS_18_X,
+      entry: 'lib/api/login.ts',
+      handler: 'handler',
+      environment:{
+        "USERNAME": databaseSecret.secretValueFromJson('username').unsafeUnwrap().toString(),
+        "PASSWORD": databaseSecret.secretValueFromJson('password').unsafeUnwrap().toString()
+      }
+    });
+
+    const loginLambdaIntegration = new aws_apigateway.LambdaIntegration(
+      loginLambda
+    );
+
+    // Register Lambda
+
+    // Login Lambda
+
+    const registerLambda = new aws_lambda_nodejs.NodejsFunction(this, 'backend-register', {
+      runtime: aws_lambda.Runtime.NODEJS_18_X,
+      entry: 'lib/api/register.ts',
+      handler: 'handler',
+      environment:{
+        "USERNAME": databaseSecret.secretValueFromJson('username').unsafeUnwrap().toString(),
+        "PASSWORD": databaseSecret.secretValueFromJson('password').unsafeUnwrap().toString()
+      }
+    });
+
+    const registerLambdaIntegration = new aws_apigateway.LambdaIntegration(
+      registerLambda
+    );
 
     // API Gateway
 
@@ -113,6 +145,10 @@ export class Team11BackendStack extends Stack {
 
     const loginUrl = rootUrl
       .addResource('login')
-      .addMethod('GET', loginLambdaIntegration) // <-- Should be post
+      .addMethod('POST', loginLambdaIntegration) // <-- Should be post
+
+    const registerUrl = rootUrl
+      .addResource('register')
+      .addMethod('POST', registerLambdaIntegration)
   }
 }
