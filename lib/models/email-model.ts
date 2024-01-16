e31918cf121ba6c0ca9294b97e5c1e7d4701da06
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-01-16 16:15:55 +0000

    change to string

--- lib/models/email-model.ts
+++ lib/models/email-model.ts
@@ -5,20 +5,20 @@ const EMAIL_MODEL: ModelOptions = {
   contentType: 'application/json',
   modelName: 'EmailModel',
   schema: {
-    schema: aws_apigateway.JsonSchemaVersion.DRAFT7,
+    schema: aws_apigateway.JsonSchemaVersion.DRAFT4,
     title: 'EmailSchema',
     type: aws_apigateway.JsonSchemaType.OBJECT,
     properties: {
       target: { type: aws_apigateway.JsonSchemaType.STRING },
-      score: { type: aws_apigateway.JsonSchemaType.INTEGER },
-      numberOfQuestions: { type: aws_apigateway.JsonSchemaType.INTEGER },
+      score: { type: aws_apigateway.JsonSchemaType.STRING },
+      numberOfQuestions: { type: aws_apigateway.JsonSchemaType.STRING },
       numberOfAnsweredQuestions: {
-        type: aws_apigateway.JsonSchemaType.INTEGER,
+        type: aws_apigateway.JsonSchemaType.STRING,
       },
-      correctAnswers: { type: aws_apigateway.JsonSchemaType.INTEGER },
-      wrongAnswers: { type: aws_apigateway.JsonSchemaType.INTEGER },
-      hintsUsed: { type: aws_apigateway.JsonSchemaType.INTEGER },
-      fiftyFiftyUsed: { type: aws_apigateway.JsonSchemaType.INTEGER },
+      correctAnswers: { type: aws_apigateway.JsonSchemaType.STRING },
+      wrongAnswers: { type: aws_apigateway.JsonSchemaType.STRING },
+      hintsUsed: { type: aws_apigateway.JsonSchemaType.STRING },
+      fiftyFiftyUsed: { type: aws_apigateway.JsonSchemaType.STRING },
     },
   },
 }
