69fc4a199fee26ffe278c4f71eb71bb688c59f90
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-01-24 19:11:09 +0000

    update formatting

--- bin/team11-backend.ts
+++ bin/team11-backend.ts
@@ -2,12 +2,16 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { Team11BackendStack } from '../lib/team11-backend-stack'
-import environment from '../lib/config/environment';
+import environment from '../lib/config/environment'
 
 const app = new cdk.App()
-new Team11BackendStack(app, `team11-${environment.environmentName}-backend-stack`, {
-  env: {
-    account: '394261647652',
-    region: 'eu-west-1',
-  },
-})
+new Team11BackendStack(
+  app,
+  `team11-${environment.environmentName}-backend-stack`,
+  {
+    env: {
+      account: '394261647652',
+      region: 'eu-west-1',
+    },
+  }
+)
