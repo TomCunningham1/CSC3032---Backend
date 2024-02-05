e7c36d4d8e1eb0c57182f79b18ecb4e048c544e1
40292229 <mholmes11@qub.ac.uk>
2024-02-05 21:08:16 +0000

    Added order by back
    Must've accidentally deleted it

--- lib/api/getResults.ts
+++ lib/api/getResults.ts
@@ -52,7 +52,7 @@ export const handler = async (event: any): Promise<LambdaResponseType> => {
     const scenarioID = data as unknown as ScenarioInterface[]
     logger.info(scenarioID[0].Id.toString())
 
-    const query2 = `SELECT Username, Score, NumberOfQuestions, NumberOfAnsweredQuestions, CorrectAnswers, WrongAnswers, HintsUsed, FiftyFiftyUsed, Time FROM Attempt WHERE ScenarioId = ${scenarioID[0].Id} `
+    const query2 = `SELECT Username, Score, NumberOfQuestions, NumberOfAnsweredQuestions, CorrectAnswers, WrongAnswers, HintsUsed, FiftyFiftyUsed, Time FROM Attempt WHERE ScenarioId = ${scenarioID[0].Id} ORDER BY Score DESC `
 
     const [data2] = await connection.query(query2)
 
