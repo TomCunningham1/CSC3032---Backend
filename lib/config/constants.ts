8429efa3c3c279319fe30bc8f2adbeb7478cdf2c
Tom Cunningham <tomcunningham@Toms-MacBook-Air.local>
2024-02-09 15:28:20 +0000

    run linter

--- lib/config/constants.ts
+++ lib/config/constants.ts
@@ -11,13 +11,15 @@ const CONTENT_TYPE_JSON = 'application/json'
 const NON_PRODUCTION_ENVIRONMENT = {
   environmentName: 'non-production',
   databaseName: 'team11_non_prod_db',
-  hostName: 'team11-non-production-database.cxlgfoh4iee5.eu-west-1.rds.amazonaws.com',
+  hostName:
+    'team11-non-production-database.cxlgfoh4iee5.eu-west-1.rds.amazonaws.com',
 }
 
 const PRODUCTION_ENVIRONMENT = {
   environmentName: 'production',
   databaseName: 'team11_prod_db',
-  hostName: 'team11-production-database.cxlgfoh4iee5.eu-west-1.rds.amazonaws.com'
+  hostName:
+    'team11-production-database.cxlgfoh4iee5.eu-west-1.rds.amazonaws.com',
 }
 
 export {
