37120ee127d787f3b602359d923b7b1cfca6d157
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-02-10 11:55:33 +0000

    create-schema

--- lib/database/create-schema.ts
+++ lib/database/create-schema.ts
@@ -4,6 +4,27 @@ import { createPool } from 'mysql2'
 import { jsonResponse } from '../utils/response-utils'
 import environment from '../config/environment'
 
+const query = `CREATE TABLE IF NOT EXISTS Scenario (
+  Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Attempt (
+  Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Username VARCHAR(255),
+  ScenarioId INT(6) UNSIGNED,
+  Score Int,
+  NumberOfQuestions int,
+  NumberOfAnsweredQuestions int,
+  CorrectAnswers int,
+  WrongAnswers int,
+  HintsUsed int,
+  FiftyFiftyUsed int,
+  Time int,
+  CONSTRAINT fk_scenario FOREIGN KEY (ScenarioId) REFERENCES Scenario(Id)
+);`
+
+
 export const handler = async (event: any): Promise<LambdaResponseType> => {
   const user = process.env.USERNAME
   const password = process.env.PASSWORD
@@ -20,27 +41,6 @@ export const handler = async (event: any): Promise<LambdaResponseType> => {
   }
 
   const conn = createPool(dbConfig).promise()
-
-  const query =
-    `USE ${environment.databaseName};` +
-    `CREATE TABLE IF NOT EXISTS Scenario (` +
-    `    Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,` +
-    `    Name VARCHAR(255)` +
-    `);` +
-    `CREATE TABLE IF NOT EXISTS Attempt (` +
-    `    Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,` +
-    `    Username VARCHAR(255),` +
-    `    ScenarioId INT(6) UNSIGNED,` +
-    `    Score INT,` +
-    `    NumberOfQuestions INT,` +
-    `    NumberOfAnsweredQuestions INT,` +
-    `    CorrectAnswers INT,` +
-    `    WrongAnswers INT,` +
-    `    HintsUsed INT,` +
-    `    FiftyFiftyUsed INT,` +
-    `    Time INT,` +
-    `    CONSTRAINT fk_scenario FOREIGN KEY (ScenarioId) REFERENCES Scenario(Id)` +
-    `)`
 
   try {
     const connection = await conn.getConnection()
