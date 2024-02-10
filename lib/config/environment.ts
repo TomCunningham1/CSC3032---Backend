3fdf163d648f3deef76d1729ab3b4b43524b571e
Tom Cunningham <tcunningham12@qub.ac.uk>
2024-02-10 15:07:36 +0000

    update function names

--- lib/config/environment.ts
+++ lib/config/environment.ts
@@ -4,17 +4,20 @@ class Environment {
   environmentName: string
   databaseName: string
   hostName: string
+  abbr: string
 
   constructor(env: string) {
     if (env === 'non-prod') {
       this.environmentName = NON_PRODUCTION_ENVIRONMENT.environmentName
       this.databaseName = NON_PRODUCTION_ENVIRONMENT.databaseName
       this.hostName = NON_PRODUCTION_ENVIRONMENT.hostName
+      this.abbr = 'np'
     }
     if (env === 'prod') {
       this.environmentName = PRODUCTION_ENVIRONMENT.environmentName
       this.databaseName = PRODUCTION_ENVIRONMENT.databaseName
       this.hostName = PRODUCTION_ENVIRONMENT.hostName
+      this.abbr = 'p'
     }
   }
 }
